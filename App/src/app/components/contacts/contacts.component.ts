fe774362db1488f22745ffd24c2d2d65530ec97c
Liam Ryan <128687090+LiamRyan1@users.noreply.github.com>
2025-03-31 13:04:34 +0100

    working on contacts page

--- App/src/app/components/contacts/contacts.component.ts
+++ App/src/app/components/contacts/contacts.component.ts
@@ -1,15 +1,40 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-contacts',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.css'
 })
 export class ContactsComponent {
   constructor(private router: Router) {}
+  name:String = "";
+  Number:String = "";
+  Email:String = "";
+  savedName:String ="";
+  savedNumber:String = "";
+
   openHome()
   {
     this.router.navigate(['home'])
   }
+  onSave()
+  {
+    alert("OnSave()");
+    if(this.name != "" && this.Number >= "" )
+    {
+      this.savedName = this.name;
+      this.savedNumber = this.Number;
+    }
+    this.name = "";
+    this.Number = "";
+  }
+  onResetForm()
+  {
+    alert("onResetForm()");
+    this.name = "";
+    this.Number = "";
+  }
+ 
 }
